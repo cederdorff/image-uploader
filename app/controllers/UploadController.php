c43cfa3cf431fad82d5bc2d9de11c78484169c6d
Rasmus Cederdorff <rasmus@cederdorff.com>
2014-05-14 14:34:59 +0200

    Initial commit, jquery-file-upload added to laravel project with save and destroy methods

---
+++ app/controllers/UploadController.php
@@ -0,0 +1,81 @@
+<?php
+
+class UploadController extends \BaseController {
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		return Response::json(array('files'=>Image::get()->toArray()));
+	}
+
+
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return Response
+	 */
+	public function save()
+	{
+		// Grab our files input
+		$files = Input::file('files');
+    // We will store our uploads in public/uploads/basic
+		$assetPath = '/uploads';
+		$uploadPath = public_path($assetPath);
+    // We need an empty arry for us to put the files back into
+		$results = array();
+
+		foreach ($files as $file) {
+
+        // set our results to have our asset path
+			$image = new image;
+			$name = $file->getClientOriginalName();
+
+			$image->name = $name;
+			$image->size = $file->getSize();
+			$image->description = $name;
+			// $image->url = $path;
+			// $image->thumbnailUrl = $path;
+			
+			// gem i database for at kunne bruge id
+			$image->save();
+
+			$imageUrl = $assetPath . '/' . $image->id . '_' . $name;
+			$thumbnailUrl = $assetPath . '/thumbnails/' . $image->id . '_' . $name;
+			$thumbnailPath = $uploadPath . '/thumbnails/' . $image->id . '_' . $name;
+
+
+			$image->url = $imageUrl;
+			$image->thumbnailUrl = $thumbnailUrl;
+			$image->save();
+
+			ImageManipulation::make($file->getRealPath())->grab(150)->save($thumbnailPath);
+			// $thumbnail->save($thumbnailPath);
+
+			$file->move($uploadPath, $imageUrl);
+		}
+
+    // return all images as json
+		return Response::json(array('files'=> array('0' => $image->toArray())));
+	}
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function destroy($id) {
+    	$image = Image::find($id);
+
+    	File::delete(public_path($image->url));
+    	File::delete(public_path($image->thumbnailUrl));
+
+        Image::destroy($id);
+
+        return Response::json(array('success' => true));
+    }
+}
---
+++ app/routes.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the Closure to execute when that URI is requested.
+|
+*/
+Route::get('/', function(){
+	return View::make('uploader');	
+});
+
+Route::group(array('prefix' => 'api'), function(){
+	// Route ressource. Kan tilgås ved /api/users
+	Route::resource('images', 'UploadController');
+});
+
+Route::post('api/images', array('uses' => 'UploadController@save'));
+// Route::get('api/images', array('uses' => 'UploadController@index'));
+// Route::delete('api/images/(:num)', array('uses' => 'UploadController@destroy'));
+
+// Route::group(array('prefix' => 'api'), function(){
+// 	// Route ressource. Kan tilgås ved /api/users
+// 	Route::resource('images', 'UploadController');
+// });
